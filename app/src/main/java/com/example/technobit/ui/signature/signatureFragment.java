17f94eb726c57be333f2141febb113ce4ae80bd7
rconfa <riccardoconfalonieri98@gmail.com>
2020-03-25 16:07:52 +0100

    Import fragment view from https://github.com/zahid-ali-shah/SignatureView. Start implementing signature fragment. Use fragment_signature instead of send_signature in mobile_navigation for app testing. Update androidx implementation with the new version.

--- app/src/main/java/com/example/technobit/ui/signature/signatureFragment.java
+++ app/src/main/java/com/example/technobit/ui/signature/signatureFragment.java
@@ -1,38 +1,50 @@
 package com.example.technobit.ui.signature;
 
-import androidx.lifecycle.ViewModelProviders;
-
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.technobit.R;
 
 public class signatureFragment extends Fragment {
 
     private SignatureViewModel mViewModel;
+    private EditText et_description;
 
-    public static signatureFragment newInstance() {
-        return new signatureFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        mViewModel =
+                ViewModelProviders.of(this).get(SignatureViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_signature, container, false);
+
+        et_description = (EditText) root.findViewById(R.id.et_eventDesc);
+
+
+        et_description.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    hideKeyboard();
+                }
+            }
+        });
+
+        return root;
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_signature, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SignatureViewModel.class);
-        // TODO: Use the ViewModel
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(et_description.getWindowToken(), 0);
     }
 
 }
