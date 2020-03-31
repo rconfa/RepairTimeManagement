65d34ecbbd91435d53c5d680a8732fe59bbb2af2
rconfa <riccardoconfalonieri98@gmail.com>
2020-03-31 19:25:08 +0200

    Add event to calendar using the new utility class.

--- app/src/main/java/com/example/technobit/ui/signature/SignatureFragment.java
+++ app/src/main/java/com/example/technobit/ui/signature/SignatureFragment.java
@@ -1,7 +1,9 @@
 package com.example.technobit.ui.signature;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,36 +15,51 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.preference.PreferenceManager;
 
 import com.example.technobit.R;
 import com.example.technobit.ui.customize.signatureview.SignatureView;
+import com.example.technobit.utilities.GoogleCalendarUtility;
+import com.example.technobit.utilities.SmartphoneControlUtility;
 
-public class signatureFragment extends Fragment {
+public class SignatureFragment extends Fragment {
 
     private SignatureViewModel mViewModel;
     private EditText et_description;
     private Button btn_send;
     private Button btn_clear;
     private SignatureView signatureView;
+    private SharedPreferences sharedPref;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         mViewModel = ViewModelProviders.of(this).get(SignatureViewModel.class);
 
+        // view of the layout
         View root = inflater.inflate(R.layout.fragment_signature, container, false);
 
-        // prendo tutti gli oggetti ui necessari
-        // bottone per pulire la firma
-        btn_clear = (Button) root.findViewById(R.id.btn_clear);
-        // bottone per inviare tutti i dati!
-        btn_send  = (Button) root.findViewById(R.id.btn_send);
-        // edit text con la descrizione
-        et_description = (EditText) root.findViewById(R.id.et_eventDesc);
-        // signature View per la firma del cliente
-        signatureView = (SignatureView) root.findViewById(R.id.signature_view);
+        Bundle bundle = this.getArguments();
+        // get bundle values
+        final long endMillis = bundle.getLong("endMillis", -1);
+        final long startMillis =  bundle.getLong("startMillis", -1);
+        final String eventTitle = bundle.getString("EventTitle", "");
 
-        // evento sul click per pulire la firma
+        // Shared preference for get/set all the preference
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        
+        
+        // get all UI object
+        // Button to clear the signature
+        btn_clear = (Button) root.findViewById(R.id.btn_clear);
+        // Button for send all data to calendar
+        btn_send  = (Button) root.findViewById(R.id.btn_send);
+        // edittext for event description
+        et_description = (EditText) root.findViewById(R.id.et_eventDesc);
+        // signatureView for client signature
+        signatureView = root.findViewById(R.id.signature_view);
+
+        // Event on click on button "clear"
         btn_clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,16 +67,29 @@ public class signatureFragment extends Fragment {
             }
         });
 
-        // evento sul click per inviare tutto
+        // Event on click on button "send"
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendOnCalendar();
+                String desc = et_description.getText().toString();
+                String email = sharedPref.getString(getString(R.string.shared_email), null);
+                int color = getColorInt();
+                GoogleCalendarUtility gCal = new GoogleCalendarUtility(eventTitle, desc, email,
+                        startMillis, endMillis, color, getParentFragment());
+                boolean res = gCal.sendOnCalendar();
+                if(!res) {
+                    SmartphoneControlUtility scu = new SmartphoneControlUtility(getContext(), true);
+                    scu.shake();
+                }
+                else{
+                    // TODO: turn back to the calling fragment
+                }
             }
+
         });
 
 
-        // se cambia il focus chiudo la tastiera
+        // If the editText no longer as the focus I close the keyboard
         et_description.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
             @Override
@@ -73,20 +103,44 @@ public class signatureFragment extends Fragment {
         return root;
     }
 
-    // se la tastiera è aperta la chiudo!
+    // Close the keyboard if open
     private void hideKeyboard() {
         InputMethodManager imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(et_description.getWindowToken(), 0);
     }
 
-    // pulisco tutto il canvas
+    // Clear the canvas
     private void clearCanvas(){
         signatureView.clearCanvas();
     }
 
-    // ottengo l'immagine
+    // get the signature image
     private Bitmap getImage(){
         return signatureView.getSignatureBitmap();
+    }
+
+
+    private int getColorInt(){
+        String def_color = getResources().getString(R.string.default_color_str);
+        int defaultColorValue = Color.parseColor(def_color);
+
+        // Get the selected color from the shared preference
+        int color_selected = sharedPref.getInt(getString(R.string.shared_saved_color), defaultColorValue);
+
+        int[] mColorChoices=null;
+        String[] color_array = getResources().getStringArray(R.array.default_color_choice_values);
+
+        int colorVal = -1;
+        if (color_array!=null && color_array.length>0) {
+            int i = 0;
+            while(i < color_array.length || colorVal == -1){
+                if(Color.parseColor(color_array[i]) == color_selected)
+                    colorVal = ++i;
+                ++i;
+            }
+        }
+        // If I don't find the color return 1 (default color) else the right color val
+        return colorVal == -1 ?  1 : colorVal;
     }
 
     private void sendOnCalendar(){
