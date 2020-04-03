65f634550a52b33bdc43060a187ae5d7be626202
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-03 17:56:51 +0200

    Implemented event insert using new google api.

--- app/src/main/java/com/example/technobit/utilities/GoogleCalendarUtility.java
+++ app/src/main/java/com/example/technobit/utilities/GoogleCalendarUtility.java
@@ -1,109 +1,107 @@
 package com.example.technobit.utilities;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.CalendarContract;
+import android.os.AsyncTask;
 
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+
+import java.io.IOException;
+import java.util.Collections;
 import java.util.TimeZone;
 
-public class GoogleCalendarUtility {
-    private String mEventTitle, mEventDescription, mSelectedEmail;
+public class GoogleCalendarUtility extends AsyncTask<String, Void, String> {
+    private final Calendar mService;
+    private String mEventTitle, mEventDescription;
     private long mStartMillis, mEndMillis;
     private int mEventColor;
     private Fragment mCallingFragment;
     private Context mContext;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     // constructor with parameters
-    public GoogleCalendarUtility(String mEventTitle, String mEventDescription, String mSelectedEmail,
+    public GoogleCalendarUtility(String mEventTitle, String mEventDescription,
                                  long mStartMillis, long mEndMillis, int mEventColor,
                                  Fragment mCallingFragment) {
         this.mEventTitle = mEventTitle;
         this.mEventDescription = mEventDescription;
-        this.mSelectedEmail = mSelectedEmail;
         this.mStartMillis = mStartMillis;
         this.mEndMillis = mEndMillis;
         this.mEventColor = mEventColor;
         this.mCallingFragment = mCallingFragment;
         this.mContext = this.mCallingFragment.getContext();
+
+        final NetHttpTransport HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
+        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(mContext,
+                Collections.singleton(CalendarScopes.CALENDAR))
+                .setBackOff(new ExponentialBackOff());
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(mContext);
+        credential.setSelectedAccount(account.getAccount());
+        mService =  new com.google.api.services.calendar.Calendar.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Technobit")
+                .build();
+
     }
 
-    public boolean sendOnCalendar() {
-        ContentResolver cr = mContext.getContentResolver();
-        String calID = searchID(cr); // search for the calendar id
+    void insertEvent() throws IOException {
+        Event event = new Event()
+                .setSummary(mEventTitle)
+                .setDescription(mEventDescription)
+                .setColorId(Integer.toString(mEventColor));
 
-        if (!calID.equals("-1")) {
-            TimeZone tz = TimeZone.getDefault();
+        // overload reminders, I want no reminders!
+        Event.Reminders reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(null);
+        event.setReminders(reminders);
 
-            ContentValues values = new ContentValues();
 
-            // mettere controllo id!=-1
-            values.put(CalendarContract.Events.DTSTART, mStartMillis);
-            values.put(CalendarContract.Events.DTEND, mEndMillis);
-            values.put(CalendarContract.Events.TITLE, mEventTitle);
-            values.put(CalendarContract.Events.DESCRIPTION, mEventDescription);
-            values.put(CalendarContract.Events.CALENDAR_ID, calID);
-            values.put(CalendarContract.Events.EVENT_TIMEZONE, tz.getID());
-            values.put(CalendarContract.Events.EVENT_COLOR_KEY, mEventColor);
+        TimeZone tz = TimeZone.getDefault();
 
-            //Try to add the event on calendar
-            try {
-                if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_CALENDAR) !=
-                        PackageManager.PERMISSION_GRANTED) {
-                    mCallingFragment.requestPermissions(new String[]{Manifest.permission.WRITE_CALENDAR}, 0);
-                    return false;
-                } else {
-                    Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
-                    return true;
-                }
-            } catch (Exception e) {
-                return false;
-            }
-        }
-        return false;
+        java.util.Calendar c = java.util.Calendar.getInstance();
+        DateTime endDate = new DateTime(c.getTime());
+
+        c.setTimeInMillis(c.getTimeInMillis() - mStartMillis);
+        DateTime startDate = new DateTime(c.getTime());
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDate)
+                .setTimeZone(tz.getID());
+        event.setStart(start);
+
+
+        EventDateTime end = new EventDateTime()
+                .setDateTime(endDate)
+                .setTimeZone(tz.getID());
+        event.setEnd(end);
+
+        String calendarId = "primary";
+        //event.send
+        if(mService!=null)
+            mService.events().insert(calendarId, event).execute();
     }
 
-
-    private String searchID(ContentResolver cr) {
-        // Search the calendar id
-        String projection[] = {"_id"}; // richiedo l'id del calendario
-
-        // Set the args in base the email address
-        String[] selectionArgs = new String[]{this.mSelectedEmail};
-
-        // Execute the query requesting the field name like the one specified in the selectionArgs
-        Cursor managedCursor = cr.query(Uri.parse("content://com.android.calendar/calendars"),
-                projection, "calendar_displayName=?", selectionArgs, null);
-
-        // Scan all the calendars
-        String calID = "-1";
-
+    @Override
+    protected String doInBackground(String... strings) {
         try {
-            managedCursor.moveToFirst();// Try to handle the first element
-
-            calID = managedCursor.getString(0); // getting is id
-
-            managedCursor.close();
-        } catch (NullPointerException ne) {
-            return "-1";
-        } catch (Exception e) {
-            return "-1";
+            insertEvent(); // insert the event into calendar
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return calID;
+
+        return null;
     }
 
-    public String getSelectedEmail() {
-        return mSelectedEmail;
-    }
-
-    public void setSelectedEmail(String mSelectedEmail) {
-        this.mSelectedEmail = mSelectedEmail;
-    }
 }
