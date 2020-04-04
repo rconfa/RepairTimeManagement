31a0c25982da9abdcea51f6361e895be81d61eaa
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-04 14:35:23 +0200

    Moved into folder and renamed.

--- app/src/main/java/com/example/technobit/utilities/AsyncResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.technobit.utilities;
-
-public interface AsyncResponse {
-    void processFinish(String output);
-}
---
+++ app/src/main/java/com/example/technobit/utilities/googleService/GoogleAsyncResponse.java
@@ -0,0 +1,5 @@
+package com.example.technobit.utilities.googleService;
+
+public interface GoogleAsyncResponse {
+    void processFinish(String output);
+}
