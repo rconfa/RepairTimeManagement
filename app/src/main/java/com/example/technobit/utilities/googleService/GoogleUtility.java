69108a90177acba0b8f972c02461b4a0ca66108e
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-04 16:38:22 +0200

    Implemented new utility method.

--- app/src/main/java/com/example/technobit/utilities/googleService/GoogleUtility.java
+++ app/src/main/java/com/example/technobit/utilities/googleService/GoogleUtility.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.calendar.CalendarScopes;
@@ -14,6 +17,13 @@ import java.util.Arrays;
 public class GoogleUtility {
     private Context mContext;
     private static GoogleUtility instance;
+
+    public static synchronized GoogleUtility getInstance(Context mContext){
+        if(instance==null){
+            instance=new GoogleUtility(mContext);
+        }
+        return instance;
+    }
 
     private GoogleUtility(Context mContext) {
         this.mContext = mContext;
@@ -31,11 +41,26 @@ public class GoogleUtility {
                 .setBackOff(new ExponentialBackOff());
     }
 
-    public static synchronized GoogleUtility getInstance(Context mContext){
-        if(instance==null){
-            instance=new GoogleUtility(mContext);
-        }
-        return instance;
+    // sign out from account
+    public void signOut() {
+        getSignInClient().signOut();
     }
+
+    // revoke all access for the account
+    public void revokeAccess() {
+        getSignInClient().revokeAccess();
+    }
+
+    // GoogleSignInClient
+    public GoogleSignInClient getSignInClient(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(CalendarScopes.CALENDAR), new Scope(DriveScopes.DRIVE)) // Scope to read/write calendar and drive
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        return GoogleSignIn.getClient(mContext, gso);
+    }
+
 
 }
