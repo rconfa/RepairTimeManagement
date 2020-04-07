3ca2d67adbc97d31fe3e2345880a09d62c976170
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-07 17:08:37 +0200

    Improve implementation to save data on both case drive+calendar not sent or only calendar not sent.

--- app/src/main/java/com/example/technobit/utilities/notSendedData/DataToSend.java
+++ app/src/main/java/com/example/technobit/utilities/notSendedData/DataToSend.java
@@ -13,37 +13,53 @@ import androidx.annotation.NonNull;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/*  This dataToSend class create a file to store values that are not sended to google for exception error.
+    The file has two syntax:
+    1) isDriveSent = true, if the image is already sent to drive. I need to save only data to create the event on calendar
+    true;EventTitle;EventDescription;eventDuration;eventEndTime;attachments
+    2) isDriveSent = false, if the image is not sent to drive.
+    false;EventTitle;EventDescription;eventDuration;eventEndTime;imagePath
+
+ */
 public class DataToSend {
-    String mEventTitle, mDescription, mImagePath; // title, descripion and signature image for the event
-    Long mEventStart, mEventEnd; // start and end time
+    String mEventTitle, mDescription, mImageData; // title, descripion and signature image for the event
+    Long mEventDuration, mEventEnd; // start and end time
+    Boolean mIsDriveSent; // true if the image is already upload on drive, false otherwise
 
     // empty constructor
     public DataToSend() {
     }
 
     // constructor with parameters
-    public DataToSend(String mEventTitle, String mDescription, String mImagePath, Long mEventStart, Long mEventEnd) {
+    public DataToSend( Boolean isDriveSent, String mEventTitle, String mDescription, Long mEventDuration,
+                      Long mEventEnd, String mImageData) {
         this.mEventTitle = mEventTitle;
         this.mDescription = mDescription;
-        this.mImagePath = mImagePath;
-        this.mEventStart = mEventStart;
+        this.mImageData = mImageData;
+        this.mEventDuration = mEventDuration;
         this.mEventEnd = mEventEnd;
+        this.mIsDriveSent = isDriveSent;
     }
 
     @NonNull
     @Override
     public String toString() {
-        return mEventTitle + ";" + mDescription + ";" + mEventStart + ";" + mEventEnd + ";" + mImagePath;
+        return mIsDriveSent + ";" + mEventTitle + ";" + mDescription + ";" + mEventDuration + ";"
+                + mEventEnd + ";" + mImageData;
     }
 
     // restore all data from a string
     public void readFromString(String datas){
         String[] unzippedData = datas.split(";");
-        this.mEventTitle = unzippedData[0];
-        this.mDescription = unzippedData[1];
-        this.mImagePath = unzippedData[4];
-        this.mEventStart = Long.decode(unzippedData[2]);
-        this.mEventEnd = Long.decode(unzippedData[3]);
+
+        this.mEventTitle = unzippedData[1];
+        this.mDescription = unzippedData[2];
+        this.mEventDuration = Long.decode(unzippedData[3]);
+        this.mEventEnd = Long.decode(unzippedData[4]);
+        if(unzippedData[0].equals("false")) // not sent to drive
+            this.mImageData = unzippedData[5]; // image data = only the image path
+        else
+            this.mImageData = unzippedData[5] + ";" + unzippedData[6] + ";" + unzippedData[7]; // image data = all the attachments
     }
 
     public void saveOnFile(Context mContext) throws IOException {
