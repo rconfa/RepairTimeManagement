adda1490d2f3aa7e9742d350134abf71067bf6ea
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-24 17:15:16 +0200

    Added method to check internet connection

--- app/src/main/java/com/example/technobit/utilities/SmartphoneControlUtility.java
+++ app/src/main/java/com/example/technobit/utilities/SmartphoneControlUtility.java
@@ -1,6 +1,8 @@
 package com.example.technobit.utilities;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
@@ -23,5 +25,20 @@ public class SmartphoneControlUtility {
             ((Vibrator) mContext.getSystemService(mContext.VIBRATOR_SERVICE)).vibrate(600);
         }
 
+    }
+
+    public boolean checkInternetConnection(){
+        ConnectivityManager manager =(ConnectivityManager) this.mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI){
+                return true;
+            }
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
