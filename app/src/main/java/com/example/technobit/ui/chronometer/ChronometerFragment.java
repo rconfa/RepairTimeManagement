abeb65457ae43e9e07d6e5467ed430c3781b97c9
rconfa <riccardoconfalonieri98@gmail.com>
2020-04-24 19:20:30 +0200

    Variable renaming and navigation fixed by using a variable

--- app/src/main/java/com/example/technobit/ui/chronometer/ChronometerFragment.java
+++ app/src/main/java/com/example/technobit/ui/chronometer/ChronometerFragment.java
@@ -19,7 +19,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.Navigation;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.preference.PreferenceManager;
 
 import com.example.technobit.R;
@@ -38,16 +39,16 @@ import java.util.Date;
 public class ChronometerFragment extends Fragment{
     private static final String TAG = "ChronometerFragment";
     private ChronometerViewModel chronometerViewModel;
-    private Chronometer chronometer;
-    private long pauseOffset;
-    private boolean running; // mi dice se il cronometro sta runnando
-    private FloatingActionButton fabChronoPlay, fabStop;
-    private Spinner sp;
-    private String EventTitle = "";
-    private int spinnerSelectionPos;
-    private Animation animBlink;
-    private SharedPreferences sharedPref;
-
+    private Chronometer mChronometer;
+    private long mPauseOffset;
+    private boolean mChronometerIsRunnig; // mi dice se il cronometro sta runnando
+    private FloatingActionButton mFabChronoPlay, mFabStop;
+    private Spinner mSpinnerContact;
+    private String mEventTitle = "";
+    private int mSpinnerSelectionPos;
+    private Animation mAnimBlink;
+    private SharedPreferences mSharedPref;
+    private NavController mNavigator;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -56,7 +57,7 @@ public class ChronometerFragment extends Fragment{
         View root = inflater.inflate(R.layout.fragment_chronometer, container, false);
 
         // salvo le shared preference per gestire lettura/salvataggio delle preferenze già inserite
-        sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         // if the user don't select the email I ask if he want to choose it now.
         if (!checkAccountSelected())
@@ -66,31 +67,31 @@ public class ChronometerFragment extends Fragment{
         ContactSingleton sg = ContactSingleton.getInstance(getContext());
 
         // Get all ui object
-        chronometer = root.findViewById(R.id.chrono);
+        mChronometer = root.findViewById(R.id.chrono);
         // fab button for start/pause chronometer
-        fabChronoPlay = root.findViewById(R.id.fab_start_pause);
+        mFabChronoPlay = root.findViewById(R.id.fab_start_pause);
         // Button for stop chronometer
-        fabStop = root.findViewById(R.id.fab_stop);
+        mFabStop = root.findViewById(R.id.fab_stop);
 
         // spinner for client list
-        sp = root.findViewById(R.id.spinner_choose_client);
+        mSpinnerContact = root.findViewById(R.id.spinner_choose_client);
 
         // load chronometer animation
-        animBlink = AnimationUtils.loadAnimation(getContext(), R.anim.blink);
+        mAnimBlink = AnimationUtils.loadAnimation(getContext(), R.anim.blink);
 
 
         // Init dello spinner
-        spinnerSelectionPos = 0; // Default selected index, 0 = hint for the spinner
+        mSpinnerSelectionPos = 0; // Default selected index, 0 = hint for the spinner
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(root.getContext(),
                 R.layout.spinner_item, sg.getContactNameList());
         arrayAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         arrayAdapter.insert(getResources().getString(R.string.spinner_hint),0);
-        sp.setAdapter(arrayAdapter);
+        mSpinnerContact.setAdapter(arrayAdapter);
 
 
         // LISTENER
         // event on client choose from the spinner
-        sp.setOnItemSelectedListener(
+        mSpinnerContact.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener() {
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                         // If the view is not null (ex: rotate the screen produce null view)
@@ -98,8 +99,8 @@ public class ChronometerFragment extends Fragment{
                             // get the client name
                             TextView clientName = view.findViewById(R.id.spinnerItemTextView);
                             // set the event title as the client name
-                            EventTitle = clientName.getText().toString();
-                            spinnerSelectionPos = position; // save the spinner position
+                            mEventTitle = clientName.getText().toString();
+                            mSpinnerSelectionPos = position; // save the spinner position
                         }
                     }
 
@@ -108,12 +109,12 @@ public class ChronometerFragment extends Fragment{
 
 
         // Event on chronometer start
-        fabChronoPlay.setOnClickListener(new View.OnClickListener() {
+        mFabChronoPlay.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // check if the user have selected a client
-                if(spinnerSelectionPos != 0){
+                if(mSpinnerSelectionPos != 0){
                     // Spinner is not enable yet.
-                    sp.setEnabled(false);
+                    mSpinnerContact.setEnabled(false);
                     startChronometer(SystemClock.elapsedRealtime());
                 }
                 else{
@@ -133,12 +134,14 @@ public class ChronometerFragment extends Fragment{
 
 
         // event on chronometer stop
-        fabStop.setOnClickListener(new View.OnClickListener() {
+        mFabStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 stopChronometer();
             }
         });
+
+        mNavigator = NavHostFragment.findNavController(this);
 
         return root;
     }
@@ -147,31 +150,31 @@ public class ChronometerFragment extends Fragment{
     @Override
     public void onResume(){
 
-        int state = sharedPref.getInt("chrono_state", 2);
+        int state = mSharedPref.getInt("chrono_state", 2);
 
         // state = 2 nothing to recover
         if(state != 2) {
-            int sp_pos = sharedPref.getInt("spinner_pos", 0);
-            sp.setSelection(sp_pos); // set the choosen client
-            sp.setEnabled(false);
+            int sp_pos = mSharedPref.getInt("spinner_pos", 0);
+            mSpinnerContact.setSelection(sp_pos); // set the choosen client
+            mSpinnerContact.setEnabled(false);
 
-            running = false;
+            mChronometerIsRunnig = false;
 
             // If there is something to recover, 0=running, 1=pausa
             if (state == 0) {
-                long base = sharedPref.getLong("chronoBase", SystemClock.elapsedRealtime());
-                pauseOffset = 0;
+                long base = mSharedPref.getLong("chronoBase", SystemClock.elapsedRealtime());
+                mPauseOffset = 0;
                 startChronometer(base);
             } else if (state == 1) {
-                pauseOffset = sharedPref.getLong("timeAppStopped", 0);
-                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-                running = true;
+                mPauseOffset = mSharedPref.getLong("timeAppStopped", 0);
+                mChronometer.setBase(SystemClock.elapsedRealtime() - mPauseOffset);
+                mChronometerIsRunnig = true;
                 pauseChronometer();
             }
         }
 
         // Delete the used preference
-        sharedPref.edit().remove("chrono_state").remove("spinner_pos")
+        mSharedPref.edit().remove("chrono_state").remove("spinner_pos")
                 .remove("chronoBase").remove("timeAppStopped").apply();
 
 
@@ -181,7 +184,7 @@ public class ChronometerFragment extends Fragment{
     @Override
     public void onDestroy() {
         // If the chronometer is init() save the state
-        if(chronometer != null)
+        if(mChronometer != null)
             saveAll();
 
         super.onDestroy();
@@ -190,31 +193,31 @@ public class ChronometerFragment extends Fragment{
     @Override
     public void onPause(){
         // If the chronometer is init() save the state
-        if(chronometer != null)
+        if(mChronometer != null)
             saveAll();
         super.onPause();
     }
 
     private void saveAll(){
-        chronometer.stop();
-        SharedPreferences.Editor editor = sharedPref.edit();
+        mChronometer.stop();
+        SharedPreferences.Editor editor = mSharedPref.edit();
 
 
         // If chronometer not equal 00:00 then there is an exe to be saved
-        if(!chronometer.getText().equals("00:00")){
+        if(!mChronometer.getText().equals("00:00")){
             // save the spinner position
-            editor.putInt("spinner_pos", spinnerSelectionPos);
+            editor.putInt("spinner_pos", mSpinnerSelectionPos);
 
             // Save the chronometer base
-            editor.putLong("chronoBase", chronometer.getBase());
+            editor.putLong("chronoBase", mChronometer.getBase());
 
             // Save if the chronometer is running or is on pause
-            if(running){
+            if(mChronometerIsRunnig){
                 editor.putInt("chrono_state", 0); // 0 = running
             }
             else{ // was in pause
                 editor.putInt("chrono_state", 1); // 1 = pause
-                editor.putLong("timeAppStopped", pauseOffset);
+                editor.putLong("timeAppStopped", mPauseOffset);
             }
 
         }
@@ -228,18 +231,18 @@ public class ChronometerFragment extends Fragment{
 
     private void startChronometer(long base) {
         // start the chronometer if is not running yet
-        if (!running) {
+        if (!mChronometerIsRunnig) {
             // cancel the cronometer animation
-            chronometer.clearAnimation();
+            mChronometer.clearAnimation();
             // change the image for the button play->pause
-            fabChronoPlay.setImageResource(R.drawable.ic_pause_70dp);
+            mFabChronoPlay.setImageResource(R.drawable.ic_pause_70dp);
             // button stop can not be used during running
-            fabStop.setVisibility(View.GONE);
+            mFabStop.setVisibility(View.GONE);
             // Set the chronometer base minus the paused time
-            chronometer.setBase(base - pauseOffset);
-            chronometer.start();
-            pauseOffset = 0; // set the pause to zero.
-            running = true;
+            mChronometer.setBase(base - mPauseOffset);
+            mChronometer.start();
+            mPauseOffset = 0; // set the pause to zero.
+            mChronometerIsRunnig = true;
         }
         else { // Chronometer is running so I set it on pause
             pauseChronometer();
@@ -248,45 +251,45 @@ public class ChronometerFragment extends Fragment{
     }
 
     private void pauseChronometer() {
-        if (running) {
+        if (mChronometerIsRunnig) {
             // start the animation for the chronometer
-            chronometer.startAnimation(animBlink);
+            mChronometer.startAnimation(mAnimBlink);
             // Set button stop visible
-            fabStop.setVisibility(View.VISIBLE);
+            mFabStop.setVisibility(View.VISIBLE);
             // change the image for the button pause->play
-            fabChronoPlay.setImageResource(R.drawable.ic_play_arrow_black_70dp);
-            chronometer.stop();
+            mFabChronoPlay.setImageResource(R.drawable.ic_play_arrow_black_70dp);
+            mChronometer.stop();
             // Save the duration of the pause
-            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
-            running = false;
+            mPauseOffset = SystemClock.elapsedRealtime() - mChronometer.getBase();
+            mChronometerIsRunnig = false;
         }
     }
 
     private void stopChronometer(){
-        chronometer.stop();
+        mChronometer.stop();
 
         // clear the animation
-        chronometer.clearAnimation();
+        mChronometer.clearAnimation();
         // set the chronometer as not running
-        running = false;
+        mChronometerIsRunnig = false;
 
         // get the ended date
         Date end =  Calendar.getInstance().getTime();
         // time of duration
-        long elapsedMillis = (SystemClock.elapsedRealtime() - chronometer.getBase());
+        long elapsedMillis = (SystemClock.elapsedRealtime() - mChronometer.getBase());
 
         // Initialize the instance of CalendarEvent with the known value.
-        GoogleDataSingleton eventToSave = GoogleDataSingleton.initialize(3, EventTitle, null,
+        GoogleDataSingleton.initialize(3, mEventTitle, null,
                 null, elapsedMillis, end.getTime());
 
         // go to the signature fragment to complete the action
-        Navigation.findNavController(getView()).navigate(R.id.nav_signature);
+        mNavigator.navigate(R.id.nav_signature);
 
         // reset all the chronometer to initial values
-        pauseOffset = 0;
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        sp.setEnabled(true);
-        fabStop.setVisibility(View.GONE);
+        mPauseOffset = 0;
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mSpinnerContact.setEnabled(true);
+        mFabStop.setVisibility(View.GONE);
     }
 
     private boolean checkAccountSelected(){
@@ -306,13 +309,12 @@ public class ChronometerFragment extends Fragment{
 
     }
 
-    ConfirmChoiceDialog.NoticeDialogListener mSelectNewEmailListener =
+    private ConfirmChoiceDialog.NoticeDialogListener mSelectNewEmailListener =
             new ConfirmChoiceDialog.NoticeDialogListener() {
         @Override
         public void onDialogPositiveClick() {
             // go to the tools fragment to select email
-            Navigation.findNavController(getView()).navigate(R.id.action_nav_chrono_to_tools_rip_account);
-           // Navigation.findNavController(getView()).navigate(R.id.nav_tools);
+            mNavigator.navigate(R.id.action_nav_chrono_to_tools_rip_account);
         }
     };
 
