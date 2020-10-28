34afc5332261ddf3e1c1320da5d1a87540a492d5
rconfa <riccardoconfalonieri98@gmail.com>
2020-10-28 21:25:33 +0100

    Send image to new folder in googleDrive

--- app/src/main/java/com/technobit/repair_timer/service/google/drive/InsertToGoogleDrive.java
+++ app/src/main/java/com/technobit/repair_timer/service/google/drive/InsertToGoogleDrive.java
@@ -10,12 +10,14 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.technobit.repair_timer.R;
 import com.technobit.repair_timer.service.google.GoogleAsyncResponse;
 import com.technobit.repair_timer.service.google.GoogleUtility;
 import com.technobit.repair_timer.utils.SmartphoneControlUtility;
 
 import java.io.IOException;
+import java.util.Collections;
 
 // this class perform an image upload in google drive
 public class InsertToGoogleDrive extends Thread {
@@ -43,21 +45,47 @@ public class InsertToGoogleDrive extends Thread {
             mService = new Drive.Builder(HTTP_TRANSPORT, jsonFactory, credential)
                     .setApplicationName(mContext.getString(R.string.app_name))
                     .build();
+
         }
         else
             mService = null;
     }
 
+    private String createFolder() throws IOException {
+        File fileMetadata = new File();
+        fileMetadata.setName("firme_app");
+        fileMetadata.setMimeType("application/vnd.google-apps.folder");
+
+        File file = mService.files().create(fileMetadata)
+                .setFields("id")
+                .execute();
+        return file.getId();
+    }
+
+    private String getFolderID() throws IOException {
+        // get the id for the folder "firme_app"
+        FileList result = mService.files().list().setQ("name='firme_app'")
+                .setSpaces("drive").setFields("files(id)").execute();
+
+        // if the id of the folder not exist I create a new folder and return the id
+        if(result.getFiles().isEmpty())
+            return createFolder();
+        else
+             return result.getFiles().get(0).getId();
+    }
     private String insertImage() throws IOException {
+        String folderId = getFolderID();
         File fileMetadata = new File();
         fileMetadata.setName(this.mFilepath.getName());
+        fileMetadata.setParents(Collections.singletonList(folderId));
         FileContent mediaContent = new FileContent("image/jpeg", mFilepath);
+
 
         Drive.Files.Create fileDrive = mService.files().create(fileMetadata, mediaContent);
 
         // fileDrive.getMediaHttpUploader().setProgressListener(new CustomProgressListener());
 
-        File result = fileDrive.setFields("webViewLink").execute();
+        File result = fileDrive.setFields("webViewLink, parents").execute();
 
         // return the information that I need for add attachments on google calendar
         return result.getWebViewLink();
